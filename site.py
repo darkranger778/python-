da6a3b7f16f56f7a587cc33e0bf72b8555f1477e
Haris Hussnain <83830791+darkranger778@users.noreply.github.com>
2022-02-06 10:23:18 +0000

    Add files via upload
    this is the start of my project

---
+++ site.py
@@ -0,0 +1,18 @@
+## create class @pytest .site .test_site_path_import_module1
+## create class 'site'
+## create site class ClassName(object):
+    """docstring for ."""
+
+    def __init__(self,arg):
+        super(, ).__init__()
+        self.arg =self
+## Find root directory
+@pytest.mark.test_site_create_dir_function_module1
+## Find root directory
+@pytest.mark.test_site_create_dir_function_module1
+create_dir(directory)
+## Make a directory
+@pytest.mark.test_site_create_dir_mkdir_module1
+- `parents` set to `True`
+- `exist_ok` set to `True`## Make the destination directory
+@pytest.mark.test_site_build_function_module1
